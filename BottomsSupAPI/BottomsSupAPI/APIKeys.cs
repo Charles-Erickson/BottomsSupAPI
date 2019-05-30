fbd9ff60f786623e7d74b9b9f0bc8c2528b7ea2a
Charles Erickson <cse621@gmail.com>
2019-05-30 15:17:12 -0500

    Updated git for api and built database for token

---
+++ BottomsSupAPI/BottomsSupAPI/APIKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BottomsSupAPI
+{
+    public class APIKeys
+    {
+        public static string StripeApi = "sk_test_6Q0UE8aevAuyHAUyE04vkYLp00pMeVixGh";
+    }
+}
